d23ee0e69be8b2351fe102bbd58d7c070103b29c
Jeff Muizelaar <jmuizelaar@mozilla.com>
2015-08-31 16:54:00 -0400

    Add a de-lupuser

--- texture-perf.cc
+++ texture-perf.cc
@@ -4,6 +4,19 @@
 
 int buf[WIDTH*200];
 
+void
+delupus(void *b, size_t length)
+{
+  int sum = 0;
+  char *buf = (char*)b;
+  char *end = buf + length;
+  while (buf < end) {
+    sum += *buf;
+    buf += 0x1000;
+  }
+  printf("sum: %d\n", sum);
+}
+
 double ticksToMS;
 int main(int argc, char **argv)
 {
@@ -92,6 +105,7 @@ int main(int argc, char **argv)
 
     {
       ID3D11Texture2D *texture2;
+      D3D11_MAPPED_SUBRESOURCE map;
       LARGE_INTEGER before;
       LARGE_INTEGER after;
       QueryPerformanceCounter(&before);
@@ -111,7 +125,32 @@ int main(int argc, char **argv)
       printf("%x staging time: %.1fus\n", result, time*ticksToMS);
 
       QueryPerformanceCounter(&before);
-      D3D11_MAPPED_SUBRESOURCE map;
+      result = context->Map(texture, 0, D3D11_MAP_READ_WRITE, 0, &map);
+      QueryPerformanceCounter(&after);
+      time = after.QuadPart - before.QuadPart;
+      printf("%x staging map time: %.1fus\n", result, time*ticksToMS);
+
+      delupus(map.pData, 200*WIDTH*4);
+      QueryPerformanceCounter(&before);
+      memcpy(map.pData, buf, 200*WIDTH*4);
+      QueryPerformanceCounter(&after);
+      time = after.QuadPart - before.QuadPart;
+      printf("%x memcpy time: %.1fus\n", result, time*ticksToMS);
+
+
+      QueryPerformanceCounter(&before);
+      context->Unmap(texture, 0);
+      QueryPerformanceCounter(&after);
+      time = after.QuadPart - before.QuadPart;
+      printf("%x staging unmap time: %.1fus\n", result, time*ticksToMS);
+
+      QueryPerformanceCounter(&before);
+      context->CopyResource(texture, texture2);
+      QueryPerformanceCounter(&after);
+      time = after.QuadPart - before.QuadPart;
+      printf("%x copy time: %.1fus\n", result, time*ticksToMS);
+
+      QueryPerformanceCounter(&before);
       result = context->Map(texture, 0, D3D11_MAP_READ_WRITE, 0, &map);
       QueryPerformanceCounter(&after);
       time = after.QuadPart - before.QuadPart;
@@ -135,6 +174,7 @@ int main(int argc, char **argv)
       QueryPerformanceCounter(&after);
       time = after.QuadPart - before.QuadPart;
       printf("%x copy time: %.1fus\n", result, time*ticksToMS);
+
 
     }
 
